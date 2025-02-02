5ef4384c5c496a270614d3532b799ceb813b7228
yaakiyu <yukiaya-s@outlook.com>
2025-02-02 13:09:21 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        http://example.com/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
